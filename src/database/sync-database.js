8d019d4ce97e0964a51fa466d1cca2351febbe70
zYagoz <yagoapp2013@gmail.com>
2025-10-29 16:10:05 -0300

    correction email unique on sync-database

--- src/database/sync-database.js
+++ src/database/sync-database.js
@@ -18,7 +18,7 @@ async function syncDataBase() {
             CREATE TABLE IF NOT EXIST clients(
             id SERIAL PRIMARY KEY,
             name VARCHAR(255) NOT NULL,
-            email VARCHAR(100) NOT NULL,
+            email VARCHAR(100) UNIQUE NOT NULL,
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
             updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
             );
